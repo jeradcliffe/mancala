b7140deef4c86df861b85e86b718800448d53d61
Jacob Radcliffe <jradcli2@my.westga.edu>
2016-06-12 20:27:40 -0500

    Finished step one of refactoring ComputerPlayer and HumanPlayer classes
    // TODO: Classes ComputerPlayer and HumanPlayer share most of their
    code.
    //		 Refactor their code:
    // 		 1. Create abstract base class AbstractPlayer to implement the
    //			shared code and define abstract methods for methods without
    //			duplicate code. AbstractPlayer should implement interface Player.
    //		 2. Have ComputerPlayer and HumanPlayer extend AbstractPlayer to
    //		    implement the unshared constructor code and the abstract
    methods.

---
+++ Mancala/src/edu/westga/cs6910/mancala/model/AbstractPlayer.java
@@ -0,0 +1,47 @@
+package edu.westga.cs6910.mancala.model;
+
+/**
+ * This abstract class will implement the shared code
+ * and define the abstract methods for the methods without 
+ * duplicate code in the ComputerPlayer and HumanPlayer classes.
+ * 
+ * It will also implement the Player interface.
+ * 
+ * @author Jacob Radcliffe
+ * @version 6/12/16
+ *
+ */
+public abstract class AbstractPlayer implements Player {
+	private boolean isMyTurn;
+	private String name;
+	
+	/* (non-Javadoc)
+	 * @see edu.westga.cs6910.mancala.model.Player#getIsMyTurn()
+	 */
+	@Override
+	public boolean getIsMyTurn() {
+		return this.isMyTurn;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.westga.cs6910.mancala.model.Player#getName()
+	 */
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.westga.cs6910.mancala.model.Player#setIsMyTurn(boolean)
+	 */
+	@Override
+	public void setIsMyTurn(boolean isMyTurn) {
+		this.isMyTurn = isMyTurn;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.westga.cs6910.mancala.model.Player#takeTurn(int)
+	 */
+	@Override
+	abstract public void takeTurn(int pitChoice);
+}
